cf4c27da44592380e401e8cb63c3ab6f56927a4b
别调P <bietiaop@bietiaoPdeMacBook-Pro.local>
2024-07-20 01:41:29 +0800

    fix:抽卡记录请求

--- utils/network.js
+++ utils/network.js
@@ -1,5 +1,6 @@
 import http from 'http';
 import https from 'https';
+import fetch from 'node-fetch';
 export async function checkLatency(url) {
   let request = http;
   if (url.startsWith('https')) {
@@ -45,4 +46,21 @@ export function getQueryVariable(url, variable) {
   } catch (error) {
     return null;
   }
+}
+
+export async function fetchWithRetry(url, options, retry = 3) {
+  let err;
+  const _fetch = async (url, options, retryCount = 0) => {
+    if (retryCount > retry) {
+      throw new Error('Retry limit reached', err);
+    }
+    try {
+      return await fetch(url, options);
+    } catch (error) {
+      logger.debug(`Fetch error: ${error.message}`);
+      err = error;
+      return _fetch(url, options, retryCount + 1);
+    }
+  };
+  return _fetch(url, options);
 }
