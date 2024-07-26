e76a9ed7ed60566e997458570075a3c866c0084d
a376148946 <376148946@qq.com>
2024-07-27 00:15:58 +0800

    添加[朱鸢]伤害计算，完善驱动盘与强攻武器的计算

--- model/damage/relice.js
+++ model/damage/relice.js
@@ -10,31 +10,100 @@ import { ZZZAvatarInfo } from '../avatar.js';
  * @returns {ZZZAvatarInfo['damage_basic_properties']['bonus_detail']} 套装加成
  */
 export const relice_ability = (set_id, set_num, base_detail, bonus_detail) => {
-  switch (set_id) {
-    case '31100':
-      if (set_num >= 4) {
-        let R_DmgAdd = _.get(bonus_detail, 'R_DmgAdd', 0);
-        bonus_detail['R_DmgAdd'] = R_DmgAdd + 0.2;
-        let AttackAddedRatio = _.get(bonus_detail, 'AttackAddedRatio', 0);
-        bonus_detail['AttackAddedRatio'] = AttackAddedRatio + 0.15;
-        logger.debug('relicGetter,4,R_DmgAdd');
-      }
-      break;
-    case '32500':
-      if (set_num >= 2) {
-        let IceDmgAdd = _.get(bonus_detail, 'Ice_DmgAdd', 0);
-        bonus_detail['Ice_DmgAdd'] = IceDmgAdd + 0.1;
-        logger.debug('32500,2,Ice_DmgAdd');
-      }
-      if (set_num >= 4) {
-        let A_DmgAdd = _.get(bonus_detail, 'A_DmgAdd', 0);
-        bonus_detail['A_DmgAdd'] = A_DmgAdd + 0.4;
-        let C_DmgAdd = _.get(bonus_detail, 'C_DmgAdd', 0);
-        bonus_detail['C_DmgAdd'] = C_DmgAdd + 0.4;
-        logger.debug('32500,4,A_DmgAdd');
-        logger.debug('32500,4,C_DmgAdd');
-      }
-      break;
-  }
-  return bonus_detail;
+	switch (set_id) {
+		case '31100':
+			if (set_num >= 4) {
+				let R_DmgAdd = _.get(bonus_detail, 'R_DmgAdd', 0);
+				bonus_detail['R_DmgAdd'] = R_DmgAdd + 0.2;
+				let AttackAddedRatio = _.get(bonus_detail, 'AttackAddedRatio', 0);
+				bonus_detail['AttackAddedRatio'] = AttackAddedRatio + 0.15;
+				logger.debug('relicGetter,4,R_DmgAdd');
+			}
+			break;
+		case '32500':
+			if (set_num >= 2) {
+				let IceDmgAdd = _.get(bonus_detail, 'Ice_DmgAdd', 0);
+				bonus_detail['Ice_DmgAdd'] = IceDmgAdd + 0.1;
+				logger.debug('32500,2,Ice_DmgAdd');
+			}
+			if (set_num >= 4) {
+				let A_DmgAdd = _.get(bonus_detail, 'A_DmgAdd', 0);
+				bonus_detail['A_DmgAdd'] = A_DmgAdd + 0.4;
+				let C_DmgAdd = _.get(bonus_detail, 'C_DmgAdd', 0);
+				bonus_detail['C_DmgAdd'] = C_DmgAdd + 0.4;
+				logger.debug('32500,4,A_DmgAdd');
+				logger.debug('32500,4,C_DmgAdd');
+			}
+			break;
+		case '32600':
+			if (set_num >= 2) {
+				let PhysicalDmgAdd = _.get(bonus_detail, 'Physical_DmgAdd', 0);
+				bonus_detail['Physical_DmgAdd'] = PhysicalDmgAdd + 0.1;
+				logger.debug('32600,2,Physical_DmgAdd');
+			}
+			if (set_num >= 4) {
+				let AllDmgAdd = _.get(bonus_detail, 'All_DmgAdd', 0);
+				bonus_detail['All_DmgAdd'] = AllDmgAdd + 0.35;
+				logger.debug('32600,4,All_DmgAdd');
+			}
+			break;
+		case '32400':
+			if (set_num >= 2) {
+				let Electric_DmgAdd = _.get(bonus_detail, 'Electric_DmgAdd', 0);
+				bonus_detail['Electric_DmgAdd'] = Electric_DmgAdd + 0.1;
+				logger.debug('32400,2,Electric_DmgAdd');
+			}
+			if (set_num >= 4) {
+				let AttackAddedRatio = _.get(bonus_detail, 'AttackAddedRatio', 0);
+				bonus_detail['AttackAddedRatio'] = AttackAddedRatio + 0.28;
+				logger.debug('32400,4,AttackAddedRatio');
+			}
+			break;
+		case '32200':
+			if (set_num >= 2) {
+				let Fire_DmgAdd = _.get(bonus_detail, 'Fire_DmgAdd', 0);
+				bonus_detail['Fire_DmgAdd'] = Fire_DmgAdd + 0.1;
+				logger.debug('32200,4,Fire_DmgAdd');
+			}
+			if (set_num >= 4) {
+				let CriticalChanceBase = _.get(bonus_detail, 'CriticalChanceBase', 0);
+				bonus_detail['CriticalChanceBase'] = CriticalChanceBase + 0.28;
+				logger.debug('32200,4,CriticalChanceBase');
+			}
+			break;
+		case '32300':
+			if (set_num >= 2) {
+				let Ether_DmgAdd = _.get(bonus_detail, 'Ether_DmgAdd', 0);
+				bonus_detail['Ether_DmgAdd'] = Ether_DmgAdd + 0.1;
+				logger.debug('32300,4,Ether_DmgAdd');
+			}
+			if (set_num >= 4) {
+				let CriticalDamageBase = _.get(bonus_detail, 'CriticalDamageBase', 0);
+				bonus_detail['CriticalDamageBase'] = CriticalDamageBase + 0.53;
+				logger.debug('32300,4,CriticalDamageBase');
+			}
+			break;
+		case '31600':
+			if (set_num >= 4) {
+				let All_DmgAdd = _.get(bonus_detail, 'All_DmgAdd', 0);
+				bonus_detail['All_DmgAdd'] = All_DmgAdd + 0.53;
+				logger.debug('31600,4,All_DmgAdd');
+			}
+			break;
+		case '31400':
+			if (set_num >= 4) {
+				let AttackAddedRatio = _.get(bonus_detail, 'AttackAddedRatio', 0);
+				bonus_detail['AttackAddedRatio'] = AttackAddedRatio + 0.25;
+				logger.debug('31400,4,AttackAddedRatio');
+			}
+			break;
+		case '31000':
+			if (set_num >= 4) {
+				let AttackAddedRatio = _.get(bonus_detail, 'AttackAddedRatio', 0);
+				bonus_detail['AttackAddedRatio'] = AttackAddedRatio + 0.27;
+				logger.debug('31400,4,AttackAddedRatio');
+			}
+			break;
+	}
+	return bonus_detail;
 };
--- model/damage/weapon.js
+++ model/damage/weapon.js
@@ -11,17 +11,66 @@ const weapon_effect = getMapData('weapon_effect');
  * @returns {ZZZAvatarInfo['damage_basic_properties']['bonus_detail']} 套装加成
  */
 export const weapon_ability = (equipment, base_detail, bonus_detail) => {
-  if (equipment.id == 14119) {
-    let IceDmgAdd = _.get(bonus_detail, 'Ice_DmgAdd', 0);
-    bonus_detail['Ice_DmgAdd'] =
-      IceDmgAdd +
-      weapon_effect['14119']['Param']['IceDmgAdd'][equipment.star - 1];
-    logger.debug('14119,IceDmgAdd');
+	let equipid = equipment.id
+	switch (equipid) {
+		case 14119:{
+			let IceDmgAdd = _.get(bonus_detail, 'Ice_DmgAdd', 0);
+			bonus_detail['Ice_DmgAdd'] = IceDmgAdd + weapon_effect[equipment.id]['Param']['IceDmgAdd'][equipment.star - 1];
 
-    let CriticalChanceBase = _.get(bonus_detail, 'CriticalChanceBase', 0);
-    bonus_detail['CriticalChanceBase'] =
-      CriticalChanceBase +
-      weapon_effect['14119']['Param']['CriticalChanceBase'][equipment.star - 1];
-  }
-  return bonus_detail;
+			let CriticalChanceBase = _.get(bonus_detail, 'CriticalChanceBase', 0);
+			bonus_detail['CriticalChanceBase'] = CriticalChanceBase +weapon_effect[equipment.id]['Param']['CriticalChanceBase'][equipment.star - 1];
+			break;
+		}
+		case 14102:{
+			let Physical_DmgAdd = _.get(bonus_detail, 'Physical_DmgAdd', 0);
+			bonus_detail['Physical_DmgAdd'] = Physical_DmgAdd + weapon_effect[equipment.id]['Param']['Physical_DmgAdd'][equipment.star - 1];
+
+			let All_DmgAdd = _.get(bonus_detail, 'All_DmgAdd', 0);
+			bonus_detail['All_DmgAdd'] = All_DmgAdd + weapon_effect[equipment.id]['Param']['All_DmgAdd'][equipment.star - 1];
+			break;
+		}
+		case 14104:{
+			let AttackAddedRatio = _.get(bonus_detail, 'AttackAddedRatio', 0);
+			bonus_detail['AttackAddedRatio'] = AttackAddedRatio + weapon_effect[equipment.id]['Param']['AttackAddedRatio'][equipment.star - 1] * 8;
+			break;
+		}
+		case 14124:{
+			let A_DmgAdd = _.get(bonus_detail, 'A_DmgAdd', 0);
+			bonus_detail['A_DmgAdd'] = A_DmgAdd + weapon_effect[equipment.id]['Param']['A_DmgAdd'][equipment.star - 1] * 8;
+			break;
+		}
+		case 13001:{
+			let R_DmgAdd = _.get(bonus_detail, 'R_DmgAdd', 0);
+			bonus_detail['R_DmgAdd'] = R_DmgAdd + weapon_effect[equipment.id]['Param']['R_DmgAdd'][equipment.star - 1] * 3;
+			break;
+		}
+		case 13004:{
+			let AttackAddedRatio = _.get(bonus_detail, 'AttackAddedRatio', 0);
+			bonus_detail['AttackAddedRatio'] = AttackAddedRatio + weapon_effect[equipment.id]['Param']['AttackAddedRatio'][equipment.star - 1];
+			break;
+		}
+		case 13013:{
+			let EUP_DmgAdd = _.get(bonus_detail, 'EUP_DmgAdd', 0);
+			bonus_detail['EUP_DmgAdd'] = EUP_DmgAdd + weapon_effect[equipment.id]['Param']['EUP_DmgAdd'][equipment.star - 1];
+			break;
+		}
+		case 13106:{
+			let EUP_DmgAdd = _.get(bonus_detail, 'EUP_DmgAdd', 0);
+			bonus_detail['EUP_DmgAdd'] = EUP_DmgAdd + weapon_effect[equipment.id]['Param']['EUP_DmgAdd'][equipment.star - 1] * 15;
+			break;
+		}
+		case 13108:{
+			let Physical_DmgAdd = _.get(bonus_detail, 'Physical_DmgAdd', 0);
+			bonus_detail['Physical_DmgAdd'] = Physical_DmgAdd + weapon_effect[equipment.id]['Param']['Physical_DmgAdd'][equipment.star - 1];
+			break;
+		}
+		case 13111:{
+			let A_DmgAdd = _.get(bonus_detail, 'A_DmgAdd', 0);
+			bonus_detail['A_DmgAdd'] = A_DmgAdd + weapon_effect[equipment.id]['Param']['A_DmgAdd'][equipment.star - 1];
+			let C_DmgAdd = _.get(bonus_detail, 'C_DmgAdd', 0);
+			bonus_detail['C_DmgAdd'] = C_DmgAdd + weapon_effect[equipment.id]['Param']['A_DmgAdd'][equipment.star - 1];
+			break;
+		}
+	}
+	return bonus_detail;
 };
