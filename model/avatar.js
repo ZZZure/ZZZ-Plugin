cc9a0a1271cbf5f9d02ba5869fe8c50754b41717
bietiaop <1527109126@qq.com>
2024-07-28 23:56:23 +0800

    fix: 计算

--- model/avatar.js
+++ model/avatar.js
@@ -400,14 +400,14 @@ export class ZZZAvatarInfo {
     return damagelist;
   }
 
-  /** @type {number|boolean} */
+  /** @type {string|boolean} */
   get equip_score() {
     if (hasScoreData(this.id)) {
       let score = 0;
       for (const equip of this.equip) {
         score += equip.score;
       }
-      return score;
+      return score.toFixed(2);
     }
     return false;
   }
