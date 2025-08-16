fcb78f2426719dbb1200f0b5dec195cf4b8b5d83
Qian23333 <119576779+Qian23333@users.noreply.github.com>
2025-08-16 11:18:16 +0800

    feat: 支持整十分钟

--- apps/manage/remind.js
+++ apps/manage/remind.js
@@ -6,9 +6,17 @@ export async function setGlobalRemind() {
     this.reply('仅限主人设置', false, { at: true, recallMsg: 100 });
     return false;
   }
-  const match = this.e.msg.match(/设置全局提醒时间\s*(每日\d+时|每周.\d+时)/);
+  const match = this.e.msg.match(/设置全局提醒时间\s*(每日\d+时(?:(\d+)分)?)|每周.\d+时(?:(\d+)分)?))/);
   if (!match) return;
   const remindTime = match[1];
+  const minute = Number(match[2]) || Number(match[3]) || 0;
+
+  // 验证分钟格式
+  if (minute % 10 !== 0 || minute < 0 || minute >= 60) {
+    await this.reply('分钟必须为整十分钟');
+    return;
+  }
+
   settings.setSingleConfig('remind', 'globalRemindTime', remindTime);
   await this.reply(`全局提醒时间已更新为: ${remindTime}。`);
 }
