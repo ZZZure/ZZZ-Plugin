e70d090591d36d2c6cefa098fd25f9106bfdab48
Qian23333 <119576779+Qian23333@users.noreply.github.com>
2025-08-16 15:54:52 +0800

    优化锅巴提示

--- guoba.support.js
+++ guoba.support.js
@@ -210,7 +210,7 @@ export function supportGuoba() {
           bottomHelpMessage: '设置全局默认提醒时间，格式如"每日20时"或"每周一20时40分"',
           component: 'Input',
           componentProps: {
-            placeholder: '请输入提醒时间，如"每日20时"',
+            placeholder: '每日20时',
           },
         },
         {
