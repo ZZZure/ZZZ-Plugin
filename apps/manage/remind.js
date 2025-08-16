3823ca88117c701ce6800041d51ad2e78d52eb71
Qian23333 <119576779+Qian23333@users.noreply.github.com>
2025-08-16 10:03:26 +0800

    删注释

--- apps/manage/remind.js
+++ apps/manage/remind.js
@@ -9,9 +9,6 @@ export async function setGlobalRemind() {
   const match = this.e.msg.match(/设置全局提醒时间\s*(每日\d+时|每周.\d+时)/);
   if (!match) return;
   const remindTime = match[1];
-
-  // 使用 setSingleConfig 更新特定字段
   settings.setSingleConfig('remind', 'globalRemindTime', remindTime);
-
   await this.reply(`全局提醒时间已更新为: ${remindTime}。`);
 }
