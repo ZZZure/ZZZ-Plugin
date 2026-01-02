82ff939ae6e26c9bd4d5cf91c3bf0565a6ce2085
UCPr <2032385471@qq.com>
2026-01-03 02:04:14 +0800

    Refactor styles and update images for ranking and scoring system
    - Updated background image for the container in index.scss.
    - Refactored card and layer section styles for better organization and readability.
    - Removed obsolete RANK_SP image and added new max score images.
    - Introduced new rank background images and updated CSS to utilize them.
    - Enhanced rating and team icon styles with new images and improved structure.
    - Cleaned up CSS by consolidating repetitive styles using SCSS features.

--- apps/abyss.js
+++ apps/abyss.js
@@ -1,8 +1,7 @@
+import { rulePrefix } from '../lib/common.js';
 import { ZZZPlugin } from '../lib/plugin.js';
 import settings from '../lib/settings.js';
 import _ from 'lodash';
-import { ZZZChallenge } from '../model/abyss.js';
-import { rulePrefix } from '../lib/common.js';
 
 export class Abyss extends ZZZPlugin {
   constructor() {
@@ -34,20 +33,39 @@ export class Abyss extends ZZZPlugin {
     if (!abyssData?.hadal_ver == "v2") {
       return this.reply('式舆防卫战数据不是最新版本，可能为之前的深渊');
     }
-    if (!(abyssData?.hadal_info_v2?.fitfh_layer_detail || abyssData?.hadal_info_v2?.fourth_layer_detail)) {
-      return this.reply('没有式舆防卫战数据');
+    const data = abyssData?.hadal_info_v2;
+    if (['fitfh', 'fourth', 'third', 'second', 'first'].every(layer => !data?.[`${layer}_layer_detail`])) {
+      return this.reply('式舆防卫战数据为空');
     }
-    const abyss = new ZZZChallenge(abyssData.hadal_info_v2);
-    const timer = setTimeout(() => {
-      if (this?.reply) {
-        this.reply('查询成功，正在下载图片资源，请稍候。');
-      }
-    }, 5000);
-    await abyss.get_assets();
-    clearTimeout(timer);
+    const abyss = processAbyssData(data);
     const finalData = {
       abyss,
     };
     await this.render('abyss/index.html', finalData, this);
   }
+}
+
+function processAbyssData(abyss) {
+  const rankPercent = abyss.brief.rank_percent / 100;
+  if (rankPercent < 1) {
+    abyss.rankBg = 1;
+  } else if (rankPercent < 5) {
+    abyss.rankBg = 2;
+  } else if (rankPercent < 10) {
+    abyss.rankBg = 3;
+  } else if (rankPercent < 50) {
+    abyss.rankBg = 4;
+  } else {
+    abyss.rankBg = 5;
+  }
+  abyss.formatTime = function (time) {
+    const pad = (num) => num.toString().padStart(2, '0');
+    return `${time.year}-${pad(time.month)}-${pad(time.day)} ${pad(time.hour)}:${pad(time.minute)}:${pad(time.second)}`;
+  };
+  abyss.formatBattleTime = function (seconds) {
+    const mins = Math.floor(seconds / 60);
+    const secs = seconds % 60;
+    return `${mins.toString().padStart(2, '0')}:${secs.toString().padStart(2, '0')}`;
+  };
+  return abyss;
 }
