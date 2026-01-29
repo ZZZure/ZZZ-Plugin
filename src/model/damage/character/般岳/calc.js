5528f8fab995a8178e6d2f3e09940682372c4c31
UCPr <2032385471@qq.com>
2026-01-29 17:33:52 +0800

    改为新版伤害计算导出格式

--- src/model/damage/character/般岳/calc.js
+++ src/model/damage/character/般岳/calc.js
@@ -1,78 +1,83 @@
-/** @type {import('#interface').buff[]} */
-export const buffs = [
-  {
-    name: "1影",
-    type: "无视抗性",
-    value: 0.1,
-  },
-  {
-    name: "1影",
-    type: "贯穿增伤",
-    value: 0.1,
-  },
-  {
-    name: "2影",
-    type: "暴击伤害",
-    value: 0.15,
-  },
-  {
-    name: "4影",
-    type: "增伤",
-    value: 0.3,
-    include: ["EEAEAAE", "EAEAEEA", "AQS", "ACY"],
-  },
-  {
-    name: "6影",
-    type: "增伤",
-    value: 0.08 * 3,
-  },
-  {
-    name: "核心被动：群山如我",
-    type: "贯穿力",
-    value: ({ calc }) => Math.trunc(calc.get_HP() * 0.1 + 150 + 25 * calc.get_SkillLevel("C")),
-  },
-  {
-    name: "核心被动：群山如我",
-    type: "贯穿力",
-    value: [0.18, 0.21, 0.24, 0.27, 0.3, 0.33, 0.36],
-  },
-  {
-    name: "核心被动：群山如我",
-    type: "增伤",
-    value: [0.18, 0.21, 0.24, 0.27, 0.3, 0.33, 0.36],
-  },
-  {
-    name: "核心被动：群山如我",
-    type: "暴击伤害",
-    value: [0.18, 0.21, 0.24, 0.27, 0.3, 0.33, 0.36],
-  },
-  {
-    name: "额外能力：绝巅",
-    type: "增伤",
-    value: 0.05 * 3,
-  },
-]
+/** @type {import('#interface').CharCalcModel} */
+export default {
 
-/** @type {import('#interface').skill[]} */
-export const skills = [
-  { name: "普攻：峥嵘四段", type: "AA4" },
-  { name: "普攻：崔巍四段", type: "AE4" },
-  { name: "普攻：焚身", type: "AFS" },
-  { name: "普攻：倾山", type: "AQS" },
-  { name: "普攻：摧岳", type: "ACY" },
-  { name: "闪避反击：扬砾", type: "CF" },
-  { name: "强化特殊技：地动", type: "EAEA" },
-  { name: "强化特殊技：山摇", type: "EEEA" },
-  { name: "强化特殊技：论道", type: "EEAE" },
-  { name: "强化特殊技：狮子吼", type: "EAAE" },
-  { name: "强化特殊技：山摇·怒", type: "EAEAEEA" },
-  { name: "强化特殊技：狮子吼·怒", type: "EEAEAAE" },
-  { name: "连携技：怒焰", type: "RL" },
-  { name: "终结技：撼天动地", type: "RZ" },
-  {
-    name: "6影[额外摧岳]",
-    type: "Y6",
-    check: 6,
-    multiplier: 6,
-  },
-]
+  author: "春哥",
+
+  buffs: [
+    {
+      name: "1影",
+      type: "无视抗性",
+      value: 0.1,
+    },
+    {
+      name: "1影",
+      type: "贯穿增伤",
+      value: 0.1,
+    },
+    {
+      name: "2影",
+      type: "暴击伤害",
+      value: 0.15,
+    },
+    {
+      name: "4影",
+      type: "增伤",
+      value: 0.3,
+      include: ["EEAEAAE", "EAEAEEA", "AQS", "ACY"],
+    },
+    {
+      name: "6影",
+      type: "增伤",
+      value: 0.08 * 3,
+    },
+    {
+      name: "核心被动：群山如我",
+      type: "贯穿力",
+      value: ({ calc }) => Math.trunc(calc.get_HP() * 0.1 + 150 + 25 * calc.get_SkillLevel("C")),
+    },
+    {
+      name: "核心被动：群山如我",
+      type: "贯穿力",
+      value: [0.18, 0.21, 0.24, 0.27, 0.3, 0.33, 0.36],
+    },
+    {
+      name: "核心被动：群山如我",
+      type: "增伤",
+      value: [0.18, 0.21, 0.24, 0.27, 0.3, 0.33, 0.36],
+    },
+    {
+      name: "核心被动：群山如我",
+      type: "暴击伤害",
+      value: [0.18, 0.21, 0.24, 0.27, 0.3, 0.33, 0.36],
+    },
+    {
+      name: "额外能力：绝巅",
+      type: "增伤",
+      value: 0.05 * 3,
+    },
+  ],
+
+  skills: [
+    { name: "普攻：峥嵘四段", type: "AA4" },
+    { name: "普攻：崔巍四段", type: "AE4" },
+    { name: "普攻：焚身", type: "AFS" },
+    { name: "普攻：倾山", type: "AQS" },
+    { name: "普攻：摧岳", type: "ACY" },
+    { name: "闪避反击：扬砾", type: "CF" },
+    { name: "强化特殊技：地动", type: "EAEA" },
+    { name: "强化特殊技：山摇", type: "EEEA" },
+    { name: "强化特殊技：论道", type: "EEAE" },
+    { name: "强化特殊技：狮子吼", type: "EAAE" },
+    { name: "强化特殊技：山摇·怒", type: "EAEAEEA" },
+    { name: "强化特殊技：狮子吼·怒", type: "EEAEAAE" },
+    { name: "连携技：怒焰", type: "RL" },
+    { name: "终结技：撼天动地", type: "RZ" },
+    {
+      name: "6影[额外摧岳]",
+      type: "Y6",
+      check: 6,
+      multiplier: 6,
+    },
+  ]
+
+}
