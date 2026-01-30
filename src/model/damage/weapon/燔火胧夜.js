4f2747891b349afdf037342a70bd4337713986e1
ZM.J <zgengen@qq.com>
2026-01-30 18:23:43 +0800

    feat: Add weapons

---
+++ src/model/damage/weapon/燔火胧夜.js
@@ -0,0 +1,12 @@
+/** @type {import('#interface').buff[]} */
+export const buffs = [
+  {
+    type: "增伤",
+    value: [0.15, 0.1725, 0.195, 0.2175, 0.24],
+    element: "Fire",
+  },
+  {
+    type: "暴击率", // 装备者的生命值降低时
+    value: [0.15, 0.1725, 0.195, 0.2175, 0.24],
+  },
+]
