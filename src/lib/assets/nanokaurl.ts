a78a282009fd895a26371c3e2b76c3fecaa27aa9
UCPr <ucpr251@gmail.com>
2026-07-09 23:03:55 +0800

    更新3.0数据；补充爱芮、诺姆评分规则

--- src/lib/assets/nanokaurl.ts
+++ src/lib/assets/nanokaurl.ts
@@ -3,7 +3,7 @@ import request from '../../utils/request.js'
 export default new class {
   private readonly host = 'https://static.nanoka.cc'
   private readonly versionUrl = `${this.host}/manifest.json`
-  private version = '3.1.2+16857772'
+  private version = '3.1.4+17279672'
   private refreshPromise?: Promise<void>
 
   constructor() {
