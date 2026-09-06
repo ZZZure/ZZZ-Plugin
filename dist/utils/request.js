c94c97af5d1ef6946516540f251b17743cd8df91
重装小兔 <103594311+OverTimeBunny@users.noreply.github.com>
2026-09-06 20:01:30 +0800

    细节优化
    函数复用问题，导致拉取信息失败，已修复

--- dist/utils/request.js
+++ dist/utils/request.js
@@ -11,15 +11,12 @@ const _request = async (url, options) => {
 };
 const request = (url, options = {}, retry = 0, timeout = 15000) => {
     let err;
-    const controller = new AbortController();
-    const { signal } = controller;
-    const timeoutId = setTimeout(() => controller.abort(), timeout);
     options.headers = {
         'Content-Type': 'application/json',
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36',
         'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8',
         Accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-        'Accept-Encoding': 'gzip, deflate, br, zstd',
+        'Accept-Encoding': 'gzip, deflate',
         'Sec-Ch-Ua': '"Chromium";v="130", "Google Chrome";v="130", "Not?A_Brand";v="99"',
         'Sec-Ch-Ua-Mobile': '?0',
         'Sec-Ch-Ua-Platform': '"Windows"',
@@ -33,18 +30,18 @@ const request = (url, options = {}, retry = 0, timeout = 15000) => {
         if (retryCount > retry) {
             throw new Error('Retry limit reached', err);
         }
+        const controller = new AbortController();
+        const timeoutId = setTimeout(() => controller.abort(), timeout);
         try {
-            return await _request(url, { signal, ...options });
-        }
-        catch (error) {
+            return await _request(url, { signal: controller.signal, ...options });
+        } catch (error) {
             logger.debug(`请求失败：` +
                 (error.name === 'AbortError'
                     ? 'Request timed out '
                     : `Fetch error: ${error.message} `) + url);
             err = error;
             return await _fetch(url, options, retryCount + 1);
-        }
-        finally {
+        } finally {
             clearTimeout(timeoutId);
         }
     };
@@ -64,4 +61,3 @@ request.post = async (url, data, options = {}) => {
     return request(url, { ...restOptions, method: 'POST', body }, retry, timeout);
 };
 export default request;
-//# sourceMappingURL=request.js.map
